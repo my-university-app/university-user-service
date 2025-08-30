49275a32d09fe84a30e62cc3081e720a992b1a6f
Jeison Da silva Pereira <jeisonpereira@MacBook-Pro-de-Jeison.local>
2025-08-30 15:57:02 -0300

    First commit the app

---
+++ src/main/java/com/university/user/service/UniversityUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.university.user.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityUserServiceApplication.class, args);
+	}
+
+}
